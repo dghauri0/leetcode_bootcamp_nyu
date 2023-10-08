f17d51bb100befe70111e57ee5fe1ef8fcc42b82
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-10-08 02:14:29 -0400

    init

---
+++ week1/container_with_most_water/main.py
@@ -0,0 +1,9 @@
+from solution import Solution
+
+sol = Solution()
+
+if __name__ == '__main__':
+    height0 = [1,8,6,2,5,4,8,3,7]
+    height1 = [1,1]
+    print(sol.maxArea(height0))
+    print(sol.maxArea(height1))
