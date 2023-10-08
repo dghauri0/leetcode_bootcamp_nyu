d3296a1d227e85bfadfa31bb0aac9152ddf0a3e9
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-10-08 06:08:58 -0400

    another attempt. still seeing issues with time limit.

--- week1/sliding_window_maximum/solution.py
+++ week1/sliding_window_maximum/solution.py
@@ -1,3 +1,5 @@
+import itertools
+from collections import deque
 from typing import List
 
 
@@ -5,11 +7,13 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         left_edge = 0
         right_edge = k - 1
-        maximums = []
+        maximums = deque()
+        deqNums = deque(nums)
 
         # Outer while loop goes through all sliding iterations.
         while right_edge <= len(nums) - 1:
-            local_max = max(nums[left_edge:right_edge + 1])
+            slice = deque(itertools.islice(deqNums, left_edge, right_edge + 1))
+            local_max = max(slice)
             maximums.append(local_max)
 
             left_edge = left_edge + 1
