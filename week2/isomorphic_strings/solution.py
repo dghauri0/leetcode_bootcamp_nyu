f2fb5bd5465ca01162ee0f27d4ca5d88943ca637
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-10-11 16:27:52 -0400

    first attempt. creating dict and checking one-to-one mapping

--- week2/isomorphic_strings/solution.py
+++ week2/isomorphic_strings/solution.py
@@ -1,4 +1,25 @@
 
 class Solution:
+    # Check for one-to-one mappings. If one-to-many: false.
     def isIsomorphic(self, s: str, t: str) -> bool:
+        comp_dict = Dictionary()
+        count = 0
+        for i in s:
+            comp_dict.add(i, t[count])
+            count = count + 1
 
+        output_flag = True
+        for i in t:
+            if i not in comp_dict.values():
+                output_flag = False
+                return output_flag
+
+        return output_flag
+
+
+class Dictionary(dict):
+    def __init__(self):
+        self = dict()
+
+    def add(self, key, value):
+        self[key] = value
