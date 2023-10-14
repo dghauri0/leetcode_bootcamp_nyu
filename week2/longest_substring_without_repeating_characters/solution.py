a1b46d8d4163ec287aaddc1b33e3d44a5f37733c
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-10-14 08:14:46 -0400

    passed leetcode. starts from left and slides a window constrained by right pointer checking for duplicates in each window.

--- week2/longest_substring_without_repeating_characters/solution.py
+++ week2/longest_substring_without_repeating_characters/solution.py
@@ -2,7 +2,16 @@ class Solution:
     # Goal: Find the length of the longest substring
     #       without repeating characters.
     def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = []
         left_pointer = 0
-        right_pointer = len(s)
-        while left_pointer < right_pointer:
-            
+        output = 0
+        for right_pointer in range(len(s)):
+            while s[right_pointer] in chars:
+                chars.remove(s[left_pointer])
+                left_pointer = left_pointer + 1
+            chars.append(s[right_pointer])
+            output = max(output, right_pointer - left_pointer + 1)
+        return output
+
+
+
