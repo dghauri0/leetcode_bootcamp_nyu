03590b93ed3f47a05beb992ff91007c1bf0dc5fe
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-10-14 05:56:05 -0400

    init longest substring problem

---
+++ week2/longest_substring_without_repeating_characters/main.py
@@ -0,0 +1,13 @@
+from solution import Solution
+
+sol = Solution()
+
+if __name__ == '__main__':
+    s0 = "abcabcbb"
+    s1 = "bbbbb"
+    s2 = "pwwkew"
+    print(sol.lengthOfLongestSubstring(s0))
+    print(sol.lengthOfLongestSubstring(s1))
+    print(sol.lengthOfLongestSubstring(s1))
+
+
