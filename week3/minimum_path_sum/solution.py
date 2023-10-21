6b4e84b09df364d6761ff6d349a34bb7233f4628
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-10-21 10:10:55 -0400

    dp approach. return (0, 0) value in computed grid of minimum values.

--- week3/minimum_path_sum/solution.py
+++ week3/minimum_path_sum/solution.py
@@ -3,9 +3,13 @@ from typing import List
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        sum = float("inf")
-        local_sum = 0
-        print(sum)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                local_sum += grid[i][j]
+        m = len(grid)  # Rows
+        n = len(grid[0])  # Columns
+        min_trav_grid_dp = [[float("inf")] * (n + 1) for i in range(m + 1)]
+        min_trav_grid_dp[m][n - 1] = 0
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                min_trav_grid_dp[i][j] = min(min_trav_grid_dp[i][j + 1], min_trav_grid_dp[i + 1][j]) + grid[i][j]
+
+        return int(min_trav_grid_dp[0][0])
