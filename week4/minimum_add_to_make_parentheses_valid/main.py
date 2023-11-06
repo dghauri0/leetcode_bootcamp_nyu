e7d85bc903619d365e82b21132e5c1e47c2621f7
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-06 03:27:35 -0500

    leetcode accepted. O(n) time. O(1) space.

--- week4/minimum_add_to_make_parentheses_valid/main.py
+++ week4/minimum_add_to_make_parentheses_valid/main.py
@@ -5,8 +5,10 @@ sol = Solution()
 if __name__ == '__main__':
     s_0 = "())"
     s_1 = "((("
+    s_2 = "()))(("
     print(sol.minAddToMakeValid(s_0))
     print(sol.minAddToMakeValid(s_1))
+    print(sol.minAddToMakeValid(s_2))
 
 
 
--- week4/minimum_add_to_make_parentheses_valid/solution.py
+++ week4/minimum_add_to_make_parentheses_valid/solution.py
@@ -8,10 +8,18 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         left_open_parenth_count = 0
         right_open_parenth_count = 0
+        added = 0
+
         for i in range(len(s)):
             if s[i] == ')':
-                left_open_parenth_count += 1
+                if right_open_parenth_count > left_open_parenth_count:
+                    left_open_parenth_count += 1
+                else:
+                    added += 1
+
             if s[i] == '(':
                 right_open_parenth_count += 1
 
-        return abs(left_open_parenth_count - right_open_parenth_count)
+        added += right_open_parenth_count - left_open_parenth_count
+
+        return added
