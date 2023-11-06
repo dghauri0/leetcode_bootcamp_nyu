697ef93b3b081df7da90311e6b5855499baca07a
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-05 21:17:06 -0500

    min add parentheses init

---
+++ week4/minimum_add_to_make_parentheses_valid/main.py
@@ -0,0 +1,10 @@
+from solution import Solution
+
+sol = Solution()
+
+if __name__ == '__main__':
+    s_0 = "())"
+    s_1 = "((("
+
+
+
---
+++ week4/minimum_add_to_make_parentheses_valid/solution.py
@@ -0,0 +1,10 @@
+
+# You are given a parentheses string s. In one move, you can
+# insert a parenthesis at any position of the string.
+# For example, if s = "()))", you can insert an opening parenthesis to be "(()))"
+# or a closing parenthesis to be "())))".
+# Return the minimum number of moves required to make s valid.
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        
+        return -1
