baec84ba53ba84e5ddf81ad90262f5021a7c9883
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-11 16:21:53 -0500

    populating tree init

--- week5/binary_tree_level_order_traversal/main.py
+++ week5/binary_tree_level_order_traversal/main.py
@@ -1,12 +1,22 @@
+from typing import List
 from solution import Solution
+from treenode import TreeNode
 
 sol = Solution()
 
 if __name__ == '__main__':
-    edges_0 = [[1,2],[2,3],[4,2]]
-    edges_1 = [[1,2],[5,1],[1,3],[1,4]]
-    print(sol.findCenter(edges_0))
-    print(sol.findCenter(edges_1))
+    root_0 = [3,9,20,None,None,15,7]
+    root_0_tn = TreeNode(root_0[0], None, None)
+    for i in range(1, len(root_0), 2):
+        curr_node = root_0_tn
+        curr_node.left = root_0[i]
+        curr_node.right = root_0[i + 1]
 
+    root_1 = [1]
+    root_2 = []
+    print(sol.levelOrder(root_0))
+    print(sol.levelOrder(root_1))
+    print(sol.levelOrder(root_2))
 
-
+def populateTree(self, input: List[int]):
+    
--- week5/binary_tree_level_order_traversal/solution.py
+++ week5/binary_tree_level_order_traversal/solution.py
@@ -1,29 +1,14 @@
-from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import Optional, List
 
-# There is an undirected star graph consisting of n nodes labeled from 1 to n.
-# A star graph is a graph where there is one center node and exactly n - 1 edges
-# that connect the center node with every other node.
+from treenode import TreeNode
 
-# You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates
-# that there is an edge between the nodes ui and vi.
-# Return the center of the given star graph.
 
 class Solution:
-    def findCenter(self, edges: List[List[int]]) -> int:
-
-        # Track visited nodes.
-        visited = []
-
-        # Iterate through each edge.
-        for i in range(len(edges)):
-
-            # Iterate through each node within edge.
-            # If it is already within the visited list, return that
-            # value as the answer as the node with >= 2 connections will
-            # be the center given the definition of a star graph (we do not need
-            # to check any further).
-            for j in range(len(edges[0])):
-                if edges[i][j] in visited:
-                    return edges[i][j]
-                visited.append(edges[i][j])
-        return -1
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        return None
