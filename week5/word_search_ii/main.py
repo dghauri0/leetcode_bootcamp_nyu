41879a72d92db7ba6ce139e77cf02fe611a5d4df
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-12 04:03:28 -0500

    word search ii init, definitely a trie here

--- week5/word_search_ii/main.py
+++ week5/word_search_ii/main.py
@@ -3,10 +3,9 @@ from solution import Solution
 sol = Solution()
 
 if __name__ == '__main__':
-    edges_0 = [[1,2],[2,3],[4,2]]
-    edges_1 = [[1,2],[5,1],[1,3],[1,4]]
-    print(sol.findCenter(edges_0))
-    print(sol.findCenter(edges_1))
-
-
-
+    board_0 = [["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"], ["i", "f", "l", "v"]]
+    words_0 = ["oath", "pea", "eat", "rain"]
+    board_1 = [["a", "b"], ["c", "d"]]
+    words_1 = ["abcb"]
+    print(sol.findWords(board_0, words_0))
+    print(sol.findWords(board_1, words_1))
--- week5/word_search_ii/solution.py
+++ week5/word_search_ii/solution.py
@@ -1,29 +1,11 @@
+# Given an m x n board of characters and a list of strings words,
+# return all words on the board.
 from typing import List
 
-# There is an undirected star graph consisting of n nodes labeled from 1 to n.
-# A star graph is a graph where there is one center node and exactly n - 1 edges
-# that connect the center node with every other node.
 
-# You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates
-# that there is an edge between the nodes ui and vi.
-# Return the center of the given star graph.
-
+# Each word must be constructed from letters of sequentially adjacent cells,
+# where adjacent cells are horizontally or vertically neighboring.
+# The same letter cell may not be used more than once in a word.
 class Solution:
-    def findCenter(self, edges: List[List[int]]) -> int:
-
-        # Track visited nodes.
-        visited = []
-
-        # Iterate through each edge.
-        for i in range(len(edges)):
-
-            # Iterate through each node within edge.
-            # If it is already within the visited list, return that
-            # value as the answer as the node with >= 2 connections will
-            # be the center given the definition of a star graph (we do not need
-            # to check any further).
-            for j in range(len(edges[0])):
-                if edges[i][j] in visited:
-                    return edges[i][j]
-                visited.append(edges[i][j])
-        return -1
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        return None
