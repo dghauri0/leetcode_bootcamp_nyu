e01e35cdbb9cbde415302bcaaaeb517ff3f59288
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-12 18:54:49 -0500

    making progress. use trie for words and use in conjunction with board iteration.

--- week5/word_search_ii/solution.py
+++ week5/word_search_ii/solution.py
@@ -1,4 +1,7 @@
-from typing import List
+from typing import List, Optional
+from Trie import Trie
+
+
 # Given an m x n board of characters and a list of strings words,
 # return all words on the board.
 
@@ -7,4 +10,20 @@ from typing import List
 # The same letter cell may not be used more than once in a word.
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+
+        # Initialize and populate Trie for word list.
+        word_trie = Trie()
+        for i in words:
+            word_trie.insert(i)
+
+        board_visited = [[False] * len(board[0]) for i in range(len(board))]
+        # print(board_visited)
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if not board_visited[i][j]:
+                    board_visited[i][j] = True
+                    
         return None
+
+
+
