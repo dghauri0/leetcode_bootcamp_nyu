1f05c17f8942f999109335ab1e9de16a2e8fb623
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-13 05:35:25 -0500

    closer. issue with already visited words in board.

--- week5/word_search_ii/solution.py
+++ week5/word_search_ii/solution.py
@@ -10,6 +10,8 @@ from Trie import Trie
 # The same letter cell may not be used more than once in a word.
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # Output array.
+        output = []
 
         # Initialize and populate Trie for word list.
         word_trie = Trie()
@@ -18,28 +20,66 @@ class Solution:
 
         board_visited = [[False] * len(board[0]) for i in range(len(board))]
         # print(board_visited)
-        curr_word = ""
+
         for i in range(len(board)):
             for j in range(len(board[0])):
+                curr_word = ""
                 if not board_visited[i][j]:
                     b_i = i
                     b_j = j
                     board_visited[i][j] = True
-                    curr_word += board[i][j]
-                    dict = word_trie.query(curr_word)
+                    dict = word_trie.query(board[i][j])
+                    if len(dict) != 0:
+                        curr_word += board[i][j]
 
-                    while len(dict) != 0:
-                        if board[b_i][b_j + 1]:
-                            b_j += 1
-                        elif board[b_i][b_j - 1]:
+                    flag_right_nf = False
+                    flag_left_nf = False
+                    flag_down_nf = False
+                    flag_up_nf = False
+                    while (not flag_right_nf or not flag_left_nf or not flag_down_nf or not flag_up_nf) and len(dict) != 0:
+                        try:
+                            if board[b_i][b_j + 1]:
+                                b_j += 1
+                                if len(word_trie.query(curr_word + board[b_i][b_j])) == 0:
+                                    b_j -= 1
+                                    flag_right_nf = True
+                                else:
+                                    curr_word += board[b_i][b_j]
+                        except IndexError:
+                            # Index out of bound
+                            pass
+                        if board[b_i][b_j - 1]:
                             b_j -= 1
-                        elif board[b_i + 1][b_j]:
+                            if len(word_trie.query(curr_word + board[b_i][b_j])) == 0:
+                                b_j += 1
+                                flag_left_nf = True
+                            else:
+                                curr_word += board[b_i][b_j]
+                        if board[b_i + 1][b_j]:
                             b_i += 1
-                        elif board[b_i - 1][b_j]:
+                            if len(word_trie.query(curr_word + board[b_i][b_j])) == 0:
+                                b_i -= 1
+                                flag_down_nf = True
+                            else:
+                                curr_word += board[b_i][b_j]
+                        if board[b_i - 1][b_j]:
                             b_i -= 1
-                        dict = word_trie.query(curr_word + board[b_i][b_j])
+                            if len(word_trie.query(curr_word + board[b_i][b_j])) == 0:
+                                b_i += 1
+                                flag_up_nf = True
+                            else:
+                                curr_word += board[b_i][b_j]
 
-        return None
+                        #dict = word_trie.query(curr_word)
+
+                        if words.__contains__(curr_word):
+                            output.append(curr_word)
+                            # Maybe remove (pop) word from trie
+                            break
+
+
+
+        return output
 
 
 
