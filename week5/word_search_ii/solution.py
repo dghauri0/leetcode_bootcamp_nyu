90e63b116bf976825c81ebb97bfd1c00f720ef45
dghauri0 <59807888+dghauri0@users.noreply.github.com>
2023-11-12 19:26:38 -0500

    Update solution.py

--- week5/word_search_ii/solution.py
+++ week5/word_search_ii/solution.py
@@ -18,11 +18,27 @@ class Solution:
 
         board_visited = [[False] * len(board[0]) for i in range(len(board))]
         # print(board_visited)
+        curr_word = ""
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if not board_visited[i][j]:
+                    b_i = i
+                    b_j = j
                     board_visited[i][j] = True
-                    
+                    curr_word += board[i][j]
+                    word_trie.query(curr_word)
+
+                    # while something:
+                    if board[b_i][b_j + 1]:
+                        word_trie.query(curr_word + board[b_i][b_j + 1])
+                    elif board[b_i][b_j - 1]:
+                        word_trie.query(curr_word + board[b_i][b_j - 1])
+                    elif board[b_i + 1][b_j]:
+                        word_trie.query(curr_word + board[b_i + 1][b_j])
+                    elif board[b_i - 1][b_j]:
+                        word_trie.query(curr_word + board[b_i - 1][b_j])
+
+
         return None
 
 
